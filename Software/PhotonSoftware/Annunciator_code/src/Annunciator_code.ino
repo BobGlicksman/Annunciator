55804e4020659fdf664dfa20d4b6ba56aa3f5751
Bob Glicksman <bobg542492@aol.com>
2025-03-21 17:48:10 -0700

    Basic test code
    MP3 player is playing and LEDs are flashing.

--- Software/PhotonSoftware/Annunciator_code/src/Annunciator_code.ino
+++ Software/PhotonSoftware/Annunciator_code/src/Annunciator_code.ino
@@ -6,11 +6,7 @@
  * https://docs.particle.io/firmware/best-practices/firmware-template/
  */
 
-#define RED_LED_PIN D5
-#define GREEN_LED_PIN D6
-#define STATUS_LED_PIN D7
-
- // Include Particle Device OS APIs
+// Include Particle Device OS APIs
 #include "Particle.h"
 
 // Let Device OS manage the connection to the Particle Cloud
@@ -19,12 +15,29 @@ SYSTEM_MODE(AUTOMATIC);
 // Run the application and system concurrently in separate threads
 // SYSTEM_THREAD(ENABLED);
 
+#define RED_LED_PIN D5
+#define GREEN_LED_PIN D6
+#define STATUS_LED_PIN D7
+
+// Include the mp3 player library
+#include <DFRobotDFPlayerMini.h>
+
+// Create an instance of the miniMP3 player
+DFRobotDFPlayerMini miniMP3Player;
 
 // setup() runs once, when the device is first turned on
 void setup() {
     pinMode(GREEN_LED_PIN, OUTPUT);
     pinMode(RED_LED_PIN, OUTPUT);
     pinMode(STATUS_LED_PIN, OUTPUT);
+
+    Serial.begin(9600);
+    Serial1.begin(9600);
+
+    miniMP3Player.begin(Serial1);
+
+    miniMP3Player.volume(30);
+
 
     digitalWrite(GREEN_LED_PIN, LOW);
     digitalWrite(RED_LED_PIN, LOW);
@@ -40,6 +53,10 @@ void setup() {
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
+    for(int i = 11; i < 16; i++) {
+        miniMP3Player.playMp3Folder(i);
+        delay(5000);
+    }
     digitalWrite(GREEN_LED_PIN, HIGH);
     delay(250);
     digitalWrite(GREEN_LED_PIN, LOW);   
